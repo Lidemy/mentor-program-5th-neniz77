e24d2a008fd5babd915017d7a63269adb355ece4
neniz <3207xt@gmail.com>
2021-04-27 19:20:57 +0800

    update hw2.js

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,9 @@
 function capitalize(str) {
-  
+  let temp = 'a'
+  if(str[0] >= 'a' && str[0] <= 'z'){
+  	temp = String.fromCharCode(str.charCodeAt(0) - 32)
+  	str = str.replace(str[0], temp)
+  }
+  return str
 }
-
 console.log(capitalize('hello'));

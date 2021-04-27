a77922ad8d7cd16dce70a560e09cfbe4719bc9a1
neniz <3207xt@gmail.com>
2021-04-27 20:58:32 +0800

    update hw5 & 6

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,20 @@
 function join(arr, concatStr) {
-  
+  let result = arr[0]
+  for(var i = 1; i < arr.length; i++){
+  	result += concatStr + arr[i]
+  }
+  return result
 }
 
 function repeat(str, times) {
-  
+	var result = ''
+  for(var i = 1; i <= times; i++){
+  	result += str
+  }
+  return result
 }
 
 console.log(join(['a'], '!'));
 console.log(repeat('a', 5));
+
+

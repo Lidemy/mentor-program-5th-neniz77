b57aeb1a5602da2961de60a33e62b1b833023380
neniz <3207xt@gmail.com>
2021-04-27 21:17:47 +0800

    update self-review

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,9 +1,5 @@
 function capitalize(str) {
-  let temp = 'a'
-  if(str[0] >= 'a' && str[0] <= 'z'){
-  	temp = String.fromCharCode(str.charCodeAt(0) - 32)
-  	str = str.replace(str[0], temp)
-  }
-  return str
+  let firstCase = str[0].toUpperCase()
+  return str.replace(str[0], firstCase)
 }
 console.log(capitalize('hello'));
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,4 +1,8 @@
+//join
 function join(arr, concatStr) {
+  if(arr.length === 0) 
+    return '' // 看解答才想到，要處理空字串的狀況
+
   let result = arr[0]
   for(var i = 1; i < arr.length; i++){
   	result += concatStr + arr[i]
@@ -6,6 +10,7 @@ function join(arr, concatStr) {
   return result
 }
 
+//repeat
 function repeat(str, times) {
 	var result = ''
   for(var i = 1; i <= times; i++){
@@ -17,4 +22,5 @@ function repeat(str, times) {
 console.log(join(['a'], '!'));
 console.log(repeat('a', 5));
 
+console.log(join(["aaa", "bb", "c", "dddd"], ',,'))
 

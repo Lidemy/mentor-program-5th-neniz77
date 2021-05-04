3b7e4b8e59eed715e71a4fed150b49e8ba97f443
neniz <3207xt@gmail.com>
2021-05-04 16:24:19 +0800

    update hw1.js

---
+++ homeworks/week3/hw1.js
@@ -0,0 +1,26 @@
+var readline = require('readline');
+
+var lines = []
+var rl = readline.createInterface({
+  input: process.stdin
+});
+
+rl.on('line', function (line) { 
+  lines.push(line) 
+});
+
+rl.on('close', function() {
+  solve(lines)
+})
+
+function solve(lines) {
+  let n = lines[0]
+  printStar(n)
+}
+
+function printStar(n){
+	for(let i = 0; i < n; i++){
+		let result = '*'.repeat(i + 1)
+		console.log(result)
+	}
+}

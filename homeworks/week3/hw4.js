c88cdfb7f686c020cb920b2cc3f42f4078984790
neniz <3207xt@gmail.com>
2021-05-05 15:11:08 +0800

    update hw4.js

---
+++ homeworks/week3/hw4.js
@@ -0,0 +1,27 @@
+const readline = require('readline')
+
+const lines = []
+const rl = readline.createInterface({
+  input: process.stdin
+})
+
+rl.on('line', (line) =>
+  lines.push(line))
+
+rl.on('close', () =>
+  solve(lines))
+
+function solve(lines) {
+  const str = lines[0]
+  console.log(isPalindrome(str))
+}
+
+function isPalindrome(str) {
+  let newStr = ''
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i]
+  }
+  if (newStr === str) {
+    return 'True'
+  } else return 'False'
+}

eae483a72e7bd2abec7839e99a9f305121bff309
neniz <3207xt@gmail.com>
2021-05-06 16:17:09 +0800

    update hw3.js

--- homeworks/week3/hw3.js
+++ homeworks/week3/hw3.js
@@ -11,7 +11,7 @@ rl.on('line', (line) =>
 rl.on('close', () =>
   solve(lines))
 
-function solve(lines) {
+/* function solve(lines) {
   for (let i = 1; i < lines.length; i++) {
     console.log(primeOrComposite(Number(lines[i])))
   }
@@ -27,4 +27,26 @@ function primeOrComposite(num) {
     }
   }
   return 'Prime'
+}
+*/
+
+// 看完檢討後，覺得回傳 true / false 會讓函式更可以重複利用，所以重新寫了一個版本
+
+function solve(lines) {
+  for (let i = 1; i < lines.length; i++) {
+    const result = isPrime(Number(lines[i])) ? 'Prime' : 'Composite'
+    console.log(result)
+  }
+}
+
+function isPrime(num) {
+  if (num === 1) {
+    return false
+  }
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false
+    }
+  }
+  return true
 }

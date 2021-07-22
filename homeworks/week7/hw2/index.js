4a419f27cfe6cade53971cc0af580bd17bfc05a0
neniz <3207xt@gmail.com>
2021-07-22 10:52:47 +0800

    update hw2

--- homeworks/week7/hw2/index.js
+++ homeworks/week7/hw2/index.js
@@ -1,14 +1,12 @@
 const element = document.querySelector('.faqs')
 element.addEventListener('click', (e) => {
-  const items = document.querySelectorAll('.faq__item')
   const clicked = e.target.closest('.faq__item')
-  console.log(clicked)
-  if (!clicked.classList.contains('hide-answer')) {
-    clicked.classList.add('hide-answer')
-  } else {
-    for (const item of items) {
-      item.classList.add('hide-answer')
-    }
-    clicked.classList.remove('hide-answer')
+  const items = document.querySelectorAll('.faq__item')
+  for (const item of items) {
+    if (item === clicked) continue
+    item.classList.add('hide-answer')
+  }
+  if (clicked) {
+    clicked.classList.toggle('hide-answer')
   }
 })
